520da779a17f3f87d9e30b549fefad90c35a22aa
philippkozmin <philippkozmin@gmail.com>
2026-06-25 10:48:52 +0300

    Add second_script DAG running every 7 minutes

---
+++ dags/second_script_dag.py
@@ -0,0 +1,31 @@
+"""Второй DAG: запускается раз в 7 минут и печатает приветствие."""
+
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
+def print_hello():
+    print("hello world. I am second script")
+
+
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+}
+
+with DAG(
+    dag_id="second_script",
+    description="Печатает приветствие каждые 7 минут",
+    default_args=default_args,
+    schedule="*/7 * * * *",
+    start_date=datetime(2026, 1, 1),
+    catchup=False,
+    tags=["example"],
+) as dag:
+    hello_task = PythonOperator(
+        task_id="print_hello",
+        python_callable=print_hello,
+    )
